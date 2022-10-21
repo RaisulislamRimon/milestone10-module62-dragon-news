276f2370a022afbcaebafa9f74230c120ec7913b
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-21 10:13:25 +0600

    hello

--- dragon-news-client/src/Routes/PrivateRoute/PrivateRoute.js
+++ dragon-news-client/src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>Private Route</h1>
+    </div>
+  );
 };
 
 export default PrivateRoute;
